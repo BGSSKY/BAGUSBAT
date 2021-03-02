e07e7b45c6779cfa821cb89e0b480a45241d7939
BGSSKY <79926959+BGSSKY@users.noreply.github.com>
2021-03-03 00:18:02 +0700

    Add files via upload

---
+++ DappaUhuy/index.js
@@ -0,0 +1,9 @@
+APA LIAT LIAT PENGEN UBAH INI JUGA??
+NGOTAK KOMTOL
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+THANKS TO
+-SIZUKA BOT <ow sc>
+-DappaUhuy <dev>
+-LORD ABDUR <sensei>
+-ALL DEV BOT LAENNYA
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
